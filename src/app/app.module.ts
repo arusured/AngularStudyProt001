9c34b3623763ff311b9864aedb4aa59b72b2e7e2
kadoya.ryo <kadoya.ryo@jp.fujitsu.com>
2017-10-13 17:19:24 +0900

    info-listコンポーネントを作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InfoListComponent } from './src/app/info-list/info-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoListComponent
   ],
   imports: [
     BrowserModule
